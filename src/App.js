69453d037f8099abcc00ac17206c0ec61b9f891f
John Verdone <jmverdone@gmail.com>
2019-07-22 02:58:56 -0400

    uploaded site

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { Heading, Label, HorizontalTab } from "jmdesign";
+import Resume from "./Resume";
+import Contact from "./Contact";
+import Projects from "./Projects";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Heading text="John Verdone" heading="h1" color="dark" />
+      <Label label="Web Developer" weight="Bolder" />
+
+      <HorizontalTab
+        defaultTabIndex={1}
+        tabsData={[
+          { name: "Resume", body: <Resume />, active: true },
+          {
+            name: "Project",
+            body: <Projects />,
+            active: false
+          },
+          { name: "Contact", body: <Contact />, active: false }
+        ]}
+      />
     </div>
   );
 }
---
+++ src/Contact.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <div>
+      {" "}
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSe2sAaBqszAEZeHcbRzePifHxvYQeNrtcLVNHaUcrOLs4IzNQ/viewform?embedded=true"
+        width="640"
+        height="891"
+        frameborder="0"
+        marginheight="0"
+        marginwidth="0">
+        Loading...
+      </iframe>
+    </div>
+  );
+}
