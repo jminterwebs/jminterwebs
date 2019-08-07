7d63ba2f398ea2722deb43d4c627cd54fbb9f9f2
John Verdone <jmverdone@gmail.com>
2019-08-06 23:40:56 -0400

    remade portfolio site

--- src/App.js
+++ src/App.js
@@ -1,27 +1,55 @@
 import React from "react";
 
-import { Heading, Label, HorizontalTab } from "jmdesign";
-import Resume from "./Resume";
-import Contact from "./Contact";
-import Projects from "./Projects";
+import { Avatar, Label, Heading } from "jmdesign";
+
+import logo from "./images/jminterwebslogo.png";
+import "./styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <Heading text="John Verdone" heading="h1" color="dark" />
-      <Label label="Web Developer" weight="Bolder" />
+    <div className="wrapper">
+      <div className="header">
+        <Avatar
+          label="John M. Verdone"
+          subLabel="Full Stack Engineer"
+          initials="JV"
+          imageSrc={logo}
+        />
 
-      <HorizontalTab
-        defaultTabIndex={1}
-        tabsData={[
-          { name: "Resume", body: <Resume />, active: true },
-          {
-            name: "Project",
-            body: <Projects />,
-            active: false
-          },
-          { name: "Contact", body: <Contact />, active: false }
-        ]}
+        <div className="nav-wrapper">
+          <a href="mailto:jmverdone@gmail.com">
+            <Label
+              label="Contact"
+              weight="bolder"
+              displayStyle="inline-block"
+              labelColor="dark"
+            />
+          </a>
+
+          <a href="../public/John Verdone Resume.pdf" download>
+            <Label
+              label="Resume"
+              weight="bolder"
+              displayStyle="inline-block"
+              labelColor="dark"
+            />
+          </a>
+
+          <a href="https://github.com/jminterwebs/jmdesign" target="_blank">
+            <Label
+              label="Projects"
+              weight="bolder"
+              displayStyle="inline-block"
+              labelColor="dark"
+            />
+          </a>
+        </div>
+      </div>
+      <Heading
+        text="Under Construction"
+        size="x-large"
+        color="dark"
+        heading="h2"
       />
     </div>
   );
