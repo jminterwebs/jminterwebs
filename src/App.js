31579dcead950fa686dac149681fbdc355cfd562
John Verdone <jmverdone@gmail.com>
2020-09-09 12:32:03 -0400

    MVP front page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import { MdEmail, MdAccountCircle } from 'react-icons/md';
+import { Card, Thumbnail, Title, Label } from 'jmdesign';
+
+import logo from './images/jminterwebslogo.png';
 import './styles.scss';
 
 function App() {
-  return <div className="wrapper" />;
+  return (
+    <div className="wrapper">
+      <Card customClass="avatar-card">
+        <Title type="dark" text="John Verdone" />
+        <Thumbnail imageSrc={logo} rounded size="large" />
+        <Label type="dark" label="Full Stack Web Developer" />
+        <span>
+          <a href="http://github.com/jminterwebs">
+            <FaGithub />
+          </a>
+          <a href="https://www.linkedin.com/in/jmverdone/">
+            <FaLinkedinIn />
+          </a>
+          <a mailto="jmverdone@gmail.com">
+            <MdEmail />
+          </a>
+          <a href="../public/John Verdone Resume.pdf">
+            <MdAccountCircle />
+          </a>
+        </span>
+      </Card>
+    </div>
+  );
 }
 
 export default App;
